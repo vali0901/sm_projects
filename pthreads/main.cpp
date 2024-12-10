c0e85afb95c28f695a8476fb26c07e34f79adb54
FlaviusMazilu <maziluflavius@gmail.com>
2024-12-10 21:53:16 +0200

    add pthreads solver

--- pthreads/main.cpp
+++ pthreads/main.cpp
@@ -1,44 +1,61 @@
 #include <iostream>
 #include <pthread.h>
+#include "../utils/utils.h"
 
-const int N = 4;
-const int NUMBERS = 10;
-
-int sum = 0;
 pthread_mutex_t mutex;
+int N = 10;
 
-void* compute_sum(void* arg) {
-    int thread_id = *(int*)arg;
-    int local_sum = 0;
+struct system** systems;
+int count_systems;
+int next_system_unsolved;
 
-    for (int i = thread_id; i < NUMBERS; i += N) {
-        local_sum += i + 1;
+void* thread_run(void *args) {
+    while (true) {
+        pthread_mutex_lock(&mutex);
+        if (next_system_unsolved >= count_systems) {
+
+            pthread_mutex_unlock(&mutex);
+            return nullptr;
+        }
+        struct system* current_system = systems[next_system_unsolved++];
+    
+        pthread_mutex_unlock(&mutex);
+
+        simple_jacobi(current_system);
     }
-
-    pthread_mutex_lock(&mutex);
-    sum += local_sum;
-    pthread_mutex_unlock(&mutex);
-
     return nullptr;
-}
+};
 
 int main() {
     pthread_t threads[N];
     int thread_ids[N];
     pthread_mutex_init(&mutex, nullptr);
 
+    struct input_gen input(0, input_type::VERY_SMALL);
+    systems = input.get_input();
+    count_systems = input.no_systems;
+
+
     for (int i = 0; i < N; ++i) {
         thread_ids[i] = i;
-        pthread_create(&threads[i], nullptr, compute_sum, &thread_ids[i]);
+        pthread_create(&threads[i], nullptr, thread_run, &thread_ids[i]);
     }
+
+    thread_run(nullptr);
 
     for (int i = 0; i < N; ++i) {
         pthread_join(threads[i], nullptr);
     }
 
-    pthread_mutex_destroy(&mutex);
+    for (int i = 0; i < count_systems; i++) {
+        struct system* sys = systems[i];
+        for (int j = 0; j < sys->N; j++) {
+            std::cout << sys->x[j] << ' ';
+        }
+        std::cout << std::endl;
+    }
 
-    std::cout << "Sum of first " << NUMBERS << " numbers is: " << sum << std::endl;
+    pthread_mutex_destroy(&mutex);
 
     return 0;
 }
