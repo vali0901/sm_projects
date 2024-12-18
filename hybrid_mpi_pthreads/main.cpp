6a2193518e4984f540170fa17d6159ff8210023d
vali0901 <bobvalentin2003@gmail.com>
2024-12-18 10:44:08 +0200

    Add hybrid mpi-pthreads implementation

--- hybrid_mpi_pthreads/main.cpp
+++ hybrid_mpi_pthreads/main.cpp
@@ -1,6 +1,43 @@
+#include <mpi.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "utils.h"
+#include "messages.h"
+#include "task_pool.h"
+#include "worker.h"
+
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    if(world_rank == MASTER) {
+        struct input_gen input(0, input_type::LARGE_LARGE);
+        struct system **systems = input.get_input();
+        TaskPool task_pool(input.no_systems, world_size - 1);
+        task_pool.load_jacobi_tasks(systems);
+        task_pool.loop();
+
+        for(int i = 0; i < input.no_systems; i++)
+            if(!system_solved(systems[i])) {
+                printf("System %d not solved\n", i);
+                systems[i]->print();
+            }
+
+        for(int i = 0; i < input.no_systems; i++)
+            delete systems[i];
+        delete[] systems;
+    } else {
+        Worker worker(world_rank);
+        printf("Worker %d started\n", world_rank);
+        worker.loop();
+    }
+
+    MPI_Finalize();
     return 0;
 }
